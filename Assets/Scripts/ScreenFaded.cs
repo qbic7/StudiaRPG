be7264ff847ba11ed64f055d1b0e361f6a646c25
Dawid Kubica <kubicadawid@o2.pl>
2017-11-22 17:19:10 +0100

    Test

---
+++ Assets/Scripts/ScreenFaded.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFaded : MonoBehaviour {
+
+    Animator anim;
+    bool isFading = false;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    public IEnumerator FadeToClear()
+    {
+        isFading = true;
+        anim.SetTrigger("Fade_In");
+
+        while (isFading)
+        {
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeToBlack()
+    {
+        isFading = false;
+        anim.SetTrigger("Fade_Out");
+
+        while (isFading)
+        {
+            yield return null;
+        }
+    }
+
+    void AnimationComplete()
+    {
+        isFading = false;
+    }
+}
---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour {
+
+    public Transform warpTarget;
+
+    IEnumerator OnTriggerEnter2D(Collider2D other) {
+        ScreenFaded sf = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFaded>();
+
+        yield return StartCoroutine(sf.FadeToBlack());
+
+        Debug.Log("An object collided!");
+        other.gameObject.transform.position = warpTarget.position;
+        Camera.main.transform.position = warpTarget.position;
+
+        yield return StartCoroutine(sf.FadeToClear());
+    }
+}
